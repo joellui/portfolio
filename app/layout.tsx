88a50333afd495d2b0cdde9638fd1e54f55956a0
Joel Louis <joellui@Joels-MacBook-Pro.local>
2024-05-05 14:37:04 -0700

    initial

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,12 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
+import { Button } from "@/components/ui/button";
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { ModeToggle } from "@/components/theme-btn";
 
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -14,9 +18,46 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+        >
+          <div className="h-screen w-screen">
+
+            {children}
+
+
+
+            <footer className="fixed inset-x-0 bg-slate-100 rounded-lg shadow m-4 dark:bg-gray-800 bottom-0 opacity-50 hover:opacity-95 ease-in-out duration-300 ">
+              <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+                <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 Joel Louis Menachery All Rights Reserved.
+                </span>
+                <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+                  <li>
+                    <a href="#" className="hover:underline me-4 md:me-6">About</a>
+                  </li>
+                  <li>
+                    <a href="#" className="hover:underline me-4 md:me-6">Project</a>
+                  </li>
+                  <li>
+                    <a href="#" className="hover:underline">Contact</a>
+                  </li>
+                  <li>
+                    <ModeToggle />
+                  </li>
+
+                </ul>
+              </div>
+            </footer>
+
+          </div>
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
